757f6b087b2f2e13f4f6e30941aacd07058211aa
TerrorTurtle <te14.carl.leong@umea.nti.se>
2016-09-09 09:21:28 +0200

    Börjat med att minneslagring

--- README.md
+++ README.md
@@ -5,8 +5,9 @@ Ett terminal baserat luffarshack för 2 spelare.
 
 Log. 
 
-9.10: Börjat med minneslagring för att lagra uppgifter i spelet.
-8.58: Skapade ett github repostiary
+<p>9.10: Börjat med minneslagring för att lagra uppgifter i spelet.</p>
+
+<p>8.58: Skapade ett github repostiary</p>
 
 TODO
 Int för att spara.
--- src/Project.java
+++ src/Project.java
@@ -6,4 +6,11 @@ public class Project {
     public static final int EMPTY = 0;
     public static final int CROSS = 1;
     public static final int NOUGHT = 2;
+
+    public static final int PLAYING = 0;
+    public static final int DRAW = 1;
+    public static final int CROSS_WON = 2;
+    public static final int NOUGHT_WON = 3;
+
+    public static final int ROWS = 3, COLS = 3
 }
