b8ee1d69d0ed12d8a1a61311f31e513667578d40
TerrorTurtle <te14.carl.leong@umea.nti.se>
2016-09-09 09:14:35 +0200

    Börjat med att minneslagring

---
+++ src/Project.java
@@ -0,0 +1,9 @@
+/**
+ * Created by leca14 on 2016-09-09.
+ */
+import java.util.Scanner;
+public class Project {
+    public static final int EMPTY = 0;
+    public static final int CROSS = 1;
+    public static final int NOUGHT = 2;
+}
